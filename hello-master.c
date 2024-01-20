5798ea7243b20abedc98b30437f13aba8d1d4845
Unpraised <79714151+Unpraised0@users.noreply.github.com>
2024-01-20 17:34:00 +0700

    Update hello-master.c

--- hello-master.c
+++ hello-master.c
@@ -1,10 +1,10 @@
 //Unpraised.
 
-#include <stdio.h> // ใช้สำหรับนำเข้าโค้ดจากไฟล์อื่นเข้ามาใช้งานในโปรแกรม
+#include <stdio.h> // #include <header_file> ใช้สำหรับนำเข้าโค้ดจากไฟล์อื่นเข้ามาใช้งานในโปรแกรม
 
 int main(){
-    printf("Hello World."); // คำสั่ง printf ใช้สำหรับพิมพ์ข้อมูล ข้อมูลจากตัวแปรออกสู่หน้าจอ (Output)
-    getchar();// รอรับอินพุตจากผู้ใช้ จากนั้นก็จะหยุดการทำงานไป
+    printf("Hello World."); // printf ( "formatted_string", arguments_list) ใช้สำหรับพิมพ์ข้อมูล ข้อมูลจากตัวแปรออกสู่หน้าจอ (Output)
+    getchar();// getchar(void) รอรับอินพุตจากผู้ใช้
     
     return 0; // ส่งค่ากลับจากโปรแกรมที่ทำงานสมบูรณ์ (successful termination) หรือแสดงว่าโปรแกรมทำงานได้ถูกต้องและไม่มีปัญหาใดๆในการทำงาน.
  }
